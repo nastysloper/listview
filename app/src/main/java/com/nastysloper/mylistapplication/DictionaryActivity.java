036466b5a7a8a9efcd89e6bba083b3f5e354023d
Richard Vogt <richvogtjr@gmail.com>
2018-01-17 07:59:32 -0600

    use a map for the dictionary

--- app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
+++ app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
@@ -2,16 +2,30 @@ package com.nastysloper.mylistapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DictionaryActivity extends AppCompatActivity {
 
-    ImmutableMap<String, String> dictionary = ImmutableMap.of("welcome", "bonjour");
+    ImmutableMap<String, String> dictionary = ImmutableMap.<String, String>builder()
+            .put("welcome", "bonjour")
+            .put("dog", "chien")
+            .put("cat", "chat")
+            .put("pig", "cochon")
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+        ListView listView = (ListView) findViewById(R.id.word_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, new ArrayList<String>(dictionary.keySet()));
+        listView.setAdapter(adapter);
     }
 }
