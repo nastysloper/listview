842da77bb37897f7ce9437031b9e9b697df30d22
Richard Vogt <richvogtjr@gmail.com>
2018-01-23 08:35:54 -0600

    remove comments

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 This is app shows a list of words and the user can 
 click on a word to see it's French translation.
 
+/// Version 1
+
+Version 1 uses an in-memory list.
+
+/// Version 2
+
+Version 2 reads the list from a file.
+
--- app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
+++ app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
@@ -26,23 +26,22 @@ public class DictionaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
         ListView listView = (ListView) findViewById(R.id.word_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, new ArrayList<String>(dictionary.keySet()));
         listView.setAdapter(adapter);
+
         // you can also use a lambda here, but my version of Android Studio is running Java 7!
         // Gotta fix that...
-//        listView.setOnItemClickListener((adapterView, view, position, id) -> {
-//            String word = (String) adapterView.getItemAtPosition(position);
-//        });
+        // listView.setOnItemClickListener((adapterView, view, position, id) -> {
+        //     String word = (String) adapterView.getItemAtPosition(position);
+        // });
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 String word = (String) adapterView.getItemAtPosition(position);
-
-//                Toast toast = Toast.makeText(DictionaryActivity.this, word, Toast.LENGTH_SHORT);
-
-                // get the definition from the dictionary
                 String def = dictionary.get(word);
                 Toast toast = Toast.makeText(DictionaryActivity.this, def, Toast.LENGTH_SHORT);
 
