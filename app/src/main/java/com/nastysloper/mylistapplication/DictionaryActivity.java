ae1fb9a894e67f7bc719a025e58f74c8b27cfa72
Richard Vogt <richvogtjr@gmail.com>
2018-02-01 10:57:12 -0600

    read words from a file

--- app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
+++ app/src/main/java/com/nastysloper/mylistapplication/DictionaryActivity.java
@@ -12,7 +12,10 @@ import android.widget.Toast;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class DictionaryActivity extends AppCompatActivity {
 
@@ -59,5 +62,25 @@ public class DictionaryActivity extends AppCompatActivity {
                 // do nothing
             }
         });
+
+        Map nextDictionary = new HashMap<String, String>();
+        ListView listView1 = (ListView) findViewById(R.id.file_word_list);
+        Scanner scan = new Scanner(getResources().openRawResource(R.raw.words));
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] parts = line.split(" ");
+            nextDictionary.put(parts[0], parts[1]);
+
+        }
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, new ArrayList<String>(nextDictionary.keySet()));
+        listView1.setAdapter(adapter2);
+
+        listView1.setOnItemClickListener(((adapterView, view, i, l) -> {
+            String word1 = (String) adapterView.getItemAtPosition(i);
+            String def1 = (String) nextDictionary.get(word1);
+            Toast toast = Toast.makeText(DictionaryActivity.this, def1, Toast.LENGTH_SHORT);
+            toast.show();
+        }));
     }
 }
